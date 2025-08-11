c74abed3e9b29101a2ef9120553be9e5212ad1c4
Qamardeen Muhammad <temitopeiyioha@yahoo.com>
2025-08-11 21:07:03 +0000

    clean up example script and fix some issue

--- models/bronze/bronze_transactions.sql
+++ models/bronze/bronze_transactions.sql
@@ -1,5 +1,5 @@
-with bronze_transaction AS (
+with bronze_transactions AS (
     select CREATED_AT, INVOICE_DATE, INVOICE_NO, ORDER_ID, PAYMENT_METHOD, TOTAL_AMOUNT, UPDATED_AT
     from {{source('data_source_snowflake', 'TRANSACTIONS') }}
 )
-select * from bronze_transaction
+select * from bronze_transactions
--- models/bronze/test.sql
+++
@@ -1,4 +0,0 @@
-with test AS (
-    select * from {{source('data_source_snowflake', 'CUSTOMERS') }}
-)
-select count(*) from test
