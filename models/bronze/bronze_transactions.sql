f71c946ae0076444a0456ac2fb5550676a6c4277
Qamardeen Muhammad <temitopeiyioha@yahoo.com>
2025-08-11 20:56:04 +0000

    adding transaction file in bronze, silver as test

---
+++ models/bronze/bronze_transactions.sql
@@ -0,0 +1,5 @@
+with bronze_transaction AS (
+    select CREATED_AT, INVOICE_DATE, INVOICE_NO, ORDER_ID, PAYMENT_METHOD, TOTAL_AMOUNT, UPDATED_AT
+    from {{source('data_source_snowflake', 'TRANSACTIONS') }}
+)
+select * from bronze_transaction
