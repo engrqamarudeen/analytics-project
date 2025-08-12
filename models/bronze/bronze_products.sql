710908e51aef618ba051c60d91e7c0822d686045
mariam adewuyi <adewuyim@gmail.com>
2025-08-12 14:34:43 +0000

    moved data into bronze

---
+++ models/bronze/bronze_products.sql
@@ -0,0 +1,5 @@
+with bronze_productss AS (
+    select CREATED_AT, PRODUCT_ID, STOCK_CODE, UNIT_PRICE, CATEGORY_ID, DESCRIPTION, SUPPLIER_ID, STOCK_QUANTITY, UPDATED_AT
+    from {{source('data_source_snowflake', 'PRODUCTS') }}
+)
+select * from bronze_products
