7d24a2fb2c524cd5987689f036e9cbd93befe56d
mariam adewuyi <adewuyim@gmail.com>
2025-08-12 13:43:28 +0000

    deleted and created files under bronze

--- bronze_categories.sql
+++
@@ -1,5 +0,0 @@
-with categories AS (
-    select category_id, category_name, parent_category_id from {{source('data_source_snowflake', 'CATEGORIES') }}
-)
-insert into ANALYTIC_DWH.BRONZE.CATEGORIES
-select * from categories
