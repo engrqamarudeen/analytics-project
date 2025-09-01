5c1d4b76eddc934d32751f9a4b97a4dd345a4e14
OMOTOSO OYEKUNLE OPEYEMI <omotosooyekunle@gmail.com>
2025-09-01 19:15:42 +0000

    Help review the data for approval

--- models/Silver/Silver_order.sql
+++ models/Silver/Silver_order.sql
@@ -1,38 +1,35 @@
 -- Step 1: Get raw data from bronze_orders
-with base as (
+with
+    base as (select * from {{ ref("bronze_orders") }}),
 
-    select *
-    from {{ ref('bronze_orders') }}
+    -- Step 2: Apply cleaning and transformations
+    cleaned_order as (
 
-),
+        select
+            order_id,
+            customer_id,
+            cast(order_date as date) as order_date,
+            cast(created_at as timestamp) as created_at,
+            cast(updated_at as timestamp) as updated_at,
 
--- Step 2: Apply cleaning and transformations
-cleaned_order as (
+            billing_address,
+            shipping_address,
 
-    select
-        order_id,
-        customer_id,
-        cast(order_date as date) as order_date,
-        cast(created_at as timestamp) as created_at,
-        cast(updated_at as timestamp) as updated_at,
+            cast(discount_amount as numeric(10, 2)) as discount_amount,
+            cast(shipping_cost as numeric(10, 2)) as shipping_cost,
+            cast(tax_amount as numeric(10, 2)) as tax_amount,
+            cast(total_amount as numeric(10, 2)) as total_amount,
 
-        billing_address,
-        shipping_address,
+            order_status,
 
-        cast(discount_amount as numeric(10,2)) as discount_amount,
-        cast(shipping_cost as numeric(10,2)) as shipping_cost,
-        cast(tax_amount as numeric(10,2)) as tax_amount,
-        cast(total_amount as numeric(10,2)) as total_amount,
+            -- 🛠 Replace NULL with 'Outsourced'
+            coalesce(shipping_method, 'Outsourced') as shipping_method
 
-        order_status,
+        from base
+        where order_id is not null
 
-        -- 🛠 Replace NULL with 'Outsourced'
-        coalesce(shipping_method, 'Outsourced') as shipping_method
-
-    from base
-    where order_id is not null
-
-)
+    )
 
 -- Step 3: Final output
-select * from cleaned_order
+select *
+from cleaned_order
